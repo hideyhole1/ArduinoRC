557e28f4f10fc4b5ee9833630a9a948c18497ab5
elanlb <elan@Elans-MacBook-Pro.local>
2016-05-01 17:42:01 -0700

    Finished code
    Now the wheels work great and they also turn.

--- BB8main.ino
+++ BB8main.ino
@@ -7,7 +7,6 @@ Links for instructions:
 
 */
 
-
 //pins connected to RX
 int throttleInput = A5;
 int aileronInput = A4;
@@ -27,10 +26,43 @@ int gear;
 
 //pins connected to H-Bridge
 int m1Forward = 5;
-int m1Reverse = 6;
-int m2Forward = 10;
-int m2Reverse = 11;
+int m1Reverse = 3;
+int m2Forward = 11;
+int m2Reverse = 6;
 
+int motor1val;
+int motor2val;
+
+//1 is forward, 2 is backwards
+void spinMotor1(int fwdback, int pwm) {
+  if(fwdback == 1){
+    analogWrite(m1Forward, pwm);
+    analogWrite(m1Reverse, LOW);
+  }
+  else if(fwdback == 2){
+    analogWrite(m1Forward, LOW);
+    analogWrite(m1Reverse, pwm);
+  }
+  else {
+    analogWrite(m1Forward, LOW);
+    analogWrite(m1Reverse, LOW);
+  }
+}
+
+void spinMotor2(int fwdback, int pwm) {
+  if(fwdback == 1){
+    analogWrite(m2Forward, pwm);
+    analogWrite(m2Reverse, LOW);
+  }
+  else if(fwdback == 2){
+    analogWrite(m2Forward, LOW);
+    analogWrite(m2Reverse, pwm);
+  }
+  else {
+    analogWrite(m2Forward, LOW);
+    analogWrite(m2Reverse, LOW);
+  }
+}
 
 void setup() {
   // put your setup code here, to run once:
@@ -55,10 +87,60 @@ void loop() {
   // put your main code here, to run repeatedly:
   
   //mapping the values from 0 to 255
-  throttle = map(pulseIn(throttleInput, HIGH), 1070, 1880, 0, 255);
-  aileron = map(pulseIn(aileronInput, HIGH), 1337, 1650, 0, 255);
-  elevator = map(pulseIn(elevatorInput, HIGH), 1300, 1660, 0, 255);
-  rudder = map(pulseIn(rudderInput, HIGH), 970, 1820, 0, 255);
+  throttle = map(pulseIn(throttleInput, HIGH), 1070, 1700, 0, 180);
+  aileron = map(pulseIn(aileronInput, HIGH), 1100, 1850, 0, 255);
+  elevator = map(pulseIn(elevatorInput, HIGH), 1280, 1660, 0, 255);
+  rudder = map(pulseIn(rudderInput, HIGH), 970, 1820, 0, 180);
   
+  //make sure the values are >= 0 and <= 255
+  throttle = constrain(throttle, 0, 180);
+  aileron = constrain(aileron, 0, 255);
+  elevator = constrain(elevator, 0, 255);
+  rudder = constrain(rudder, 0, 180);
+  
+  
+  //set speeds for the motors
+  if(elevator > 135) {
+    motor1val = map(elevator, 135, 255, 0, 255);
+    motor2val = map(elevator, 135, 255, 0, 255);
+  }
+  
+  else if(elevator < 115) {
+    motor1val = map(elevator, 115, 0, 0, 255);
+    motor2val = map(elevator, 115, 0, 0, 255);
+  }
+  
+  if(aileron > 135) {
+    if(elevator > 135) {
+      motor2val = motor2val - map(aileron, 135, 255, 0, elevator - 135);
+    }
+    else if(elevator < 115) {
+      motor2val = motor2val - map(aileron, 135, 255, 0, elevator + 135);
+    }
+  }
+  
+  
+  else if(aileron < 115) {
+    if(elevator > 135) {
+      motor1val = motor1val - map(aileron, 115, 0, 0, elevator - 115);
+    }
+    else if(elevator < 115) {
+      motor1val = motor1val - map(aileron, 115, 0, 0, elevator + 115);
+    }
+  }
+  
+  //spin the motors
+  if(elevator > 135) {
+    spinMotor1(1, motor1val);
+    spinMotor2(1, motor2val);
+  }
+  else if(elevator < 115){
+    spinMotor1(2, motor1val);
+    spinMotor2(2, motor2val);
+  }
+  else {
+    spinMotor1(1, 0);
+    spinMotor2(1, 0);
+  }
   
 }
